e446142f962334a0ed008b837ced7d86deffaf0f
jin <a11718trannhuy@gmail.com>
2024-08-15 01:04:48 +0700

    remove unused cide

--- src/app/api/comment/route.ts
+++ src/app/api/comment/route.ts
@@ -22,29 +22,3 @@ export async function POST(req: NextRequest) {
         );
     };
 };
-
-// Reply to a comment
-export async function POST_REPLY(req: NextRequest) {
-    try {
-        const { commentId, reply } = await req.json();
-
-        // Reference to the comment document
-        const commentRef = doc(fireStore, "comments", commentId);
-
-        // Add the reply to the 'replies' array field
-        await updateDoc(commentRef, {
-            replies: arrayUnion(reply) // Ensure `reply` is an object with appropriate fields
-        });
-
-        return NextResponse.json(
-            { message: "Reply added successfully" },
-            { status: 200 }
-        );
-    } catch (error) {
-        console.error("Error adding reply:", error);
-        return NextResponse.json(
-            { error: "Failed to add reply" },
-            { status: 500 }
-        );
-    }
-}
