64218bd6772960a788a9a428055a8d42b8e44300
jin <a11718trannhuy@gmail.com>
2024-08-16 01:03:20 +0700

    update code

--- src/hooks/protectedRoute.tsx
+++ src/hooks/protectedRoute.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useRouter, usePathname } from "next/navigation";
-import { ReactNode, useEffect } from "react";
+import { ReactNode, useEffect, useState } from "react";
 
 interface ProtectedRouteProps {
     children: ReactNode;
@@ -10,13 +10,21 @@ interface ProtectedRouteProps {
 const ProtectedRoute = ({ children }: ProtectedRouteProps) => {
     const router = useRouter();
     const pathname = usePathname();
-    const token = localStorage.getItem("user-token");
+    const [token, setToken] = useState<string | null>(null);
 
     useEffect(() => {
+        const token = typeof window !== 'undefined' ? localStorage.getItem("user-token") : null;
+        setToken(token);
+
         if (!token && pathname !== "/login" && pathname !== "/sign-up") {
             router.replace("/login");
         }
-    }, [token, pathname, router]);
+    }, [pathname, router]);
+
+    // Handle the case where `token` is not yet determined
+    if (token === null) {
+        return null;
+    }
 
     if (!token && pathname !== "/login" && pathname !== "/sign-up") {
         return null;
