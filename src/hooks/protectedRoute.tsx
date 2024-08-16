fee850a2eac7b27be5303e5bdcb8b7b3d3fc7d5a
jin <a11718trannhuy@gmail.com>
2024-08-16 10:38:33 +0700

    update routes

--- src/hooks/protectedRoute.tsx
+++ src/hooks/protectedRoute.tsx
@@ -5,6 +5,7 @@ import { ReactNode, useEffect, useState } from "react";
 
 interface ProtectedRouteProps {
     children: ReactNode;
+    validRoutes: string[];
 }
 
 const ProtectedRoute = ({ children }: ProtectedRouteProps) => {
@@ -12,14 +13,20 @@ const ProtectedRoute = ({ children }: ProtectedRouteProps) => {
     const pathname = usePathname();
     const [token, setToken] = useState<string | null>(null);
 
+    const validRoutes = ['/login', '/sign-up', '/newsfeed'];
+
     useEffect(() => {
         const token = typeof window !== 'undefined' ? localStorage.getItem("user-token") : null;
         setToken(token);
 
-        if (!token && pathname !== "/login" && pathname !== "/sign-up") {
+        if (token && (pathname === "/login" || pathname === "/sign-up")) {
+            router.replace("/newsfeed");
+        } else if (!token && pathname !== "/login" && pathname !== "/sign-up") {
             router.replace("/login");
+        } else if (!validRoutes.includes(pathname) && !pathname.startsWith('/post/')) {
+            router.replace("/newsfeed");
         }
-    }, [pathname, router]);
+    }, [pathname, router, token, validRoutes]);
 
     if (!token && pathname !== "/login" && pathname !== "/sign-up") {
         return null;
